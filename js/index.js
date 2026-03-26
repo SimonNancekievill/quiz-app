66991feb346ee8cf9a22e5f0a78b11954433a7b4
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-03-26 17:05:56 +0100

    answer button display question answer but doesnt switch back to show answer

--- js/index.js
+++ js/index.js
@@ -1,18 +1,32 @@
-const mainContent = document.querySelector('[data-js="body-element"]');
-const user = document.querySelector('[data-js="user"]');
+// const mainContent = document.querySelector('[data-js="body-element"]');
+// const user = document.querySelector('[data-js="user"]');
+// toggleButton.addEventListener("click", () => {
+//   mainContent.classList.toggle("dark-mode");
+//   console.log("Darkmode toggle");
+// });
+
+// Bookmark Toggle
+
 const toggleButton = document.querySelector('[data-js="toggle-button"]');
 const bookmarkButton = document.querySelector('[data-js="bookmark-button"]')
-
 
 bookmarkButton.addEventListener("click", () => {
   bookmarkButton.classList.toggle("active");
 });
 
+// -----------------------
 
+// Show/Hide Answer Button
 
-toggleButton.addEventListener("click", () => {
-  mainContent.classList.toggle("dark-mode");
-  console.log("Darkmode toggle");
+const answerButton = document.querySelector('[data-js="question-button"]');
+const answer = document.querySelector('[data-js="question-answer"]');
+const answerButtonValue = answerButton.textContent;
+
+answerButton.addEventListener("click", () => {
+  answerButton.classList.toggle("active");
+  answerButton.textContent = "Hide Answer";
+  answer.classList.toggle("active");
+
 });
 
 
