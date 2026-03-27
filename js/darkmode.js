385c109e22941fe9d84a02669bd8b1d2e8e8316f
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-03-27 15:16:00 +0100

    darkmode, js toggle, small adjustments

--- js/darkmode.js
+++ js/darkmode.js
@@ -2,11 +2,15 @@
 console.clear();
 
 const mainContent = document.querySelector('[data-js="body-element"]');
+const footer = document.querySelector('[data-js="footer"]');
+const header = document.querySelector('[data-js="header"]');
 const user = document.querySelector('[data-js="user"]');
 const darkModeButton = document.querySelector('[data-js="toggle-button"]');
 
 darkModeButton.addEventListener("click", () => {
   console.log("Darkmode toggle");
   mainContent.classList.toggle("dark-mode");
+  footer.classList.toggle("footer-darkmode");
+  header.classList.toggle("header-darkmode");
 });
 // -----------------------
